29cde2dd9eef7b87d135e86c77448b749c78245c
Vaurkhorov <43547976+Vaurkhorov@users.noreply.github.com>
2023-06-17 12:32:52 +0530

    Added memory stack + Optimization
    - Added a memory stack, and its bytecode 'M' to store the answer at the following index
    - Optimized matching and redundant code

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use byteorder::{ByteOrder, LittleEndian};
 const BEGIN_OPCODE_SEARCH_RANGE: usize = 4096;
 // The length of each stack in the program
 const BUFFER_LENGTH: usize = 4096;
+// Length for the memory stack
+// This is equal to the buffer for now, but maybe it could be a lot larger later, if needed.
+const MEMORY_STACK_LENGTH: usize = BUFFER_LENGTH;
 
 
 
@@ -25,6 +28,7 @@ enum Opcode {
     IsGreater,
     Invert,
     Skip,
+    StoreAtMemoryIndex(usize),
     Unsupported(u8),
 }
 
@@ -33,6 +37,13 @@ enum Instruction {
     Opcode(Opcode),
     Number(i16),
     End,
+}
+
+enum NextInstructionStep {
+    Continue,
+    EndExecution,
+    FlushAnswerAndProceed,
+    StoreAnswerAndProceed(usize),
 }
 
 // enum OperationState {
@@ -54,6 +65,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         Ok(None) => panic!("{:?}", "Opcode::Begin not found."),
         Err(e) => panic!("{:?}", e),
     };
+    let mut memory_stack = [0i16; MEMORY_STACK_LENGTH];
+
+    // All of these stacks could be extracted into structs, and then have implementations for safe indexing
 
 
     // Execution loop
@@ -61,45 +75,95 @@ fn main() -> Result<(), Box<dyn Error>> {
         top = 0;
         input_stack = read_intructions(bytecode_path, &mut file_pointer)?;
 
-        'expression_read: for i in input_stack {
+        '_expression_read: for instruction in input_stack {
 
-
-            // This needs to be extracted into its own function, this is too much nesting
-            match i {
-                Instruction::End => {
-                    break 'expression_read;
-                },
-                Instruction::Number(t) => {
-                    operand_stack[top] = t;
-                    top += 1;
-                },
-                Instruction::Opcode(t) => {
-                    match operate(t, &mut operand_stack, &mut top) {
-                        Ok(t) => {
-                            if t == 1 || t == 2 {
-                                if top == 1 {
-                                    println!("Answer: {:?}", operand_stack[0]);
-                                }
-                                else {
-                                    println!("Operator-Operand mismatch, insufficient operations.");
-                                }
-
-                                if t == 2 {
-                                break 'bytecode_read;
-                                }
-                            }
-                        },
-                        Err(e) => println!("{:?}", e),
+            match match_instruction(instruction, &mut operand_stack, &mut top) {
+                NextInstructionStep::Continue => continue,
+                NextInstructionStep::EndExecution => break 'bytecode_read,
+                NextInstructionStep::FlushAnswerAndProceed => {
+                    if top == 1 {                                           // If there's only one number in the operand stack
+                        println!("Answer: {:?}", operand_stack[0]);
+                    }
+                    else {
+                        println!("Operator-Operand mismatch. Peeking stack: {:?}", operand_stack[top - 1]);
                     }
                 },
-
+                NextInstructionStep::StoreAnswerAndProceed(memory_index) => {
+                    if top == 1 {                                           // If there's only one number in the operand stack
+                        memory_stack[memory_index] = operand_stack[0];
+                    }
+                    else {
+                        println!("Operator-Operand mismatch. Storing: {:?} at index: {:?}", operand_stack[top - 1], memory_index);
+                        memory_stack[memory_index] = operand_stack[top - 1];
+                    }
+                },
             }
+
+
+            // This has been extracted into its own function, this was just wrong
+            // It is still here temporarily as a reference
+            //
+            //
+            // match i {
+            //     Instruction::End => {
+            //         break 'expression_read;
+            //     },
+            //     Instruction::Number(t) => {
+            //         operand_stack[top] = t;
+            //         top += 1;
+            //     },
+            //     Instruction::Opcode(t) => {
+            //         match operate(t, &mut operand_stack, &mut top) {
+            //             Ok(t) => {
+            //                 if t == 1 || t == 2 {
+            //                     if top == 1 {
+            //                         println!("Answer: {:?}", operand_stack[0]);
+            //                     }
+            //                     else {
+            //                         println!("Operator-Operand mismatch, insufficient operations.");
+            //                     }
+
+            //                     if t == 2 {
+            //                         break 'bytecode_read;
+            //                     }
+            //                 }
+            //             },
+            //             Err(e) => println!("{:?}", e),
+            //         }
+            //     },
+
+            // }
         }
     };
 
 
     Ok(())
 
+}
+
+// in the execution loop
+fn match_instruction(instruction: Instruction, operand_stack: &mut [i16; BUFFER_LENGTH], top: &mut usize) -> NextInstructionStep {
+    match instruction {
+                Instruction::End => {
+                    println!("Execution stopped, no 'End' or 'Flush' encountered.");
+                    return NextInstructionStep::EndExecution;
+                },
+                Instruction::Number(t) => {
+                    operand_stack[*top] = t;
+                    *top += 1;
+                    NextInstructionStep::Continue
+                },
+                Instruction::Opcode(t) => {
+                    match operate(t, operand_stack, top) {
+                        Ok(t) => t,
+                        Err(e) => {
+                            println!("{:?}", e);
+                            NextInstructionStep::Continue
+                        },
+                    }
+                },
+
+            }
 }
 
 fn find_start(bytecode_path: &str) -> Result<Option<usize>, Box<dyn Error>> {
@@ -149,6 +213,7 @@ fn read_intructions(bytecode_path: &str, file_pointer: &mut usize) -> Result<[In
         }
 
         else {
+
             instructions[instruction_index] = match buffer[buffer_index] {
                 b'A' => Instruction::Opcode(Opcode::Add),
                 b'S' => Instruction::Opcode(Opcode::Subtract),
@@ -164,6 +229,14 @@ fn read_intructions(bytecode_path: &str, file_pointer: &mut usize) -> Result<[In
                     println!("{:?}", decoded_num);
                     Instruction::Number(decoded_num)
                 }
+                b'M' => {
+                    let num = [buffer[buffer_index+1], buffer[buffer_index+2]];
+                    buffer_index += 2;
+                    *file_pointer += 1;
+                    let decoded_index = LittleEndian::read_i16(&num);
+                    println!("{:?}", decoded_index);
+                    Instruction::Opcode(Opcode::StoreAtMemoryIndex(decoded_index as usize % MEMORY_STACK_LENGTH))
+                }
                 t => Instruction::Opcode(Opcode::Unsupported(t)),
             }
         }
@@ -177,52 +250,57 @@ fn read_intructions(bytecode_path: &str, file_pointer: &mut usize) -> Result<[In
 
 }
 
-    fn operate(opcode: Opcode, operand_stack: &mut [i16; BUFFER_LENGTH], top: &mut usize) -> Result<u8, String>{
-        match opcode {
-            Opcode::_Begin => Err("This should never be called, it just exists for clarity.".to_owned()),
-            Opcode::FlushAndRestart => {
-                Ok(1)
-            },
-            Opcode::End => {
-                Ok(2)
-            },
-            Opcode::Add => {
+fn operate(opcode: Opcode, operand_stack: &mut [i16; BUFFER_LENGTH], top: &mut usize) -> Result<NextInstructionStep, String>{
+    match opcode {
+        Opcode::_Begin => Err("This should never be called, it just exists for clarity.".to_owned()),
 
-                // Debugging, delete these comments later
-                // println!("Add is called here.");
-                // println!("top = {:?}", *top);
-                // println!("operand_stack = {:?}", operand_stack);
+        Opcode::FlushAndRestart => {
+            Ok(NextInstructionStep::FlushAnswerAndProceed)           // Replace with NextInstructionStep
+        },
 
-                operand_stack[*top - 2] += operand_stack[*top - 1];
-                operand_stack[*top - 1] = 0;
-                *top -= 1;
-                Ok(0)
-            },
-            Opcode::Subtract => {
-                operand_stack[*top - 2] -= operand_stack[*top - 1];
-                operand_stack[*top - 1] = 0;
-                *top -= 1;
-                Ok(0)
-            },
-            Opcode::IsEqual => {
-                operand_stack[*top - 2] = (operand_stack[*top - 2] == operand_stack[*top - 1]) as i16;
-                operand_stack[*top - 1] = 0;
-                *top -= 1;
-                Ok(0)
-            },
-            Opcode::IsGreater => {
-                operand_stack[*top - 2] = (operand_stack[*top - 2] > operand_stack[*top - 1]) as i16;
-                operand_stack[*top - 1] = 0;
-                *top -= 1;
-                Ok(0)
-            },
-            Opcode::Invert => {
-                // ! returns the negative of a number when called with an i16
-                // So used `== 0` instead
-                operand_stack[*top - 1] = (operand_stack[*top - 1] == 0) as i16;
-                Ok(0)
-            },
-            Opcode::Skip => Ok(0),
-            Opcode::Unsupported(t) => Err(format!("Unrecognised Opcode: {}", t).to_owned()),
-        }
+        Opcode::End => {
+            Ok(NextInstructionStep::EndExecution)           // Replace with NextInstructionStep
+        },
+
+        Opcode::Add => {
+            operand_stack[*top - 2] += operand_stack[*top - 1];
+            operand_stack[*top - 1] = 0;
+            *top -= 1;
+            Ok(NextInstructionStep::Continue)
+        },
+
+        Opcode::Subtract => {
+            operand_stack[*top - 2] -= operand_stack[*top - 1];
+            operand_stack[*top - 1] = 0;
+            *top -= 1;
+            Ok(NextInstructionStep::Continue)
+        },
+
+        Opcode::IsEqual => {
+            operand_stack[*top - 2] = (operand_stack[*top - 2] == operand_stack[*top - 1]) as i16;
+            operand_stack[*top - 1] = 0;
+            *top -= 1;
+            Ok(NextInstructionStep::Continue)
+        },
+
+        Opcode::IsGreater => {
+            operand_stack[*top - 2] = (operand_stack[*top - 2] > operand_stack[*top - 1]) as i16;
+            operand_stack[*top - 1] = 0;
+            *top -= 1;
+            Ok(NextInstructionStep::Continue)
+        },
+
+        Opcode::Invert => {
+            // ! returns the negative of a number when called with an i16
+            // So used `== 0` instead
+            operand_stack[*top - 1] = (operand_stack[*top - 1] == 0) as i16;
+            Ok(NextInstructionStep::Continue)
+        },
+
+        Opcode::Skip => Ok(NextInstructionStep::Continue),
+
+        Opcode::Unsupported(t) => Err(format!("Unrecognised Opcode: {}", t).to_owned()),
+
+        Opcode::StoreAtMemoryIndex(index) => Ok(NextInstructionStep::StoreAnswerAndProceed(index)),
+    }
 }
